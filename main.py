243e27fbd337cd5ae708364c2eecd33eff683ac2
Toto1906 <thocarbonne@hotmail.com>
2025-04-05 18:03:10 +0200

    matrix finished

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ L = 1
 N = 5
 dxy = L/N
 
-A = np.zeros((N+1, N+1, N+1))
-
-for i in range(1, N-1):
-    for j in range(1, N-1):
-        A[i][j] = -4/(dxy**2)
-        A[i][j-1] = 1/(dxy**2)
-        A[i][j+1] = 1/(dxy**2)
-        A[i][j] = 1/(dxy**2)
-        A[i][j] = 1/(dxy**2)
-
+A = np.zeros(((N+1)**2, (N+1)**2))
+print(A.shape)
 print(A)
+
+for i in range(N+1, len(A)-N-1):
+    A[i][i] = -4/(dxy**2)
+    A[i][i-1] = 1/(dxy**2)
+    A[i][i+1] = 1/(dxy**2)
+    A[i][i-N-1] = 1/(dxy**2)
+    A[i][i+N+1] = 1/(dxy**2)
+
+print(A[10].shape)
+print(A[10])
